d76d7e7d2c8873b4b6da04f2078c18ca0f26f8a1
Anna <anna.zueva.mail@inbox.ru>
2019-09-18 21:03:08 +0300

    2.3 Рисует статистику - второй вариант исправлений
    Убрала лишние функции get...
    Координаты тени вычисляю в константе
    В renderBar передаю макс время

--- js/stat.js
+++ js/stat.js
@@ -7,12 +7,10 @@ var COLOR_BLACK = '#000';
 var fontStyle = {
   SIZE: '16px',
   FAMILY: 'PT Mono',
-  getLineHeight: function () {
-    return 1.3 * parseInt(this.SIZE, 10);
-  },
-  getFont: function () {
-    return this.SIZE + ' ' + this.FAMILY;
-  },
+  // getLineHeight: function () {
+  //   return 1.3 * parseInt(this.SIZE, 10);
+  // },
+  LINE_HEIGHT: 21,
 };
 
 // отступ от левого края "облака"
@@ -25,19 +23,28 @@ var cloudSize = {
   Y: 10,
   WIDTH: 420,
   HEIGHT: 270,
+  //
+  // getShadow: function (offset) {
+  //   var shadowSize = {};
+  //
+  //   for (var prop in this) {
+  //     if (typeof prop !== 'function') {
+  //       shadowSize[prop] = this[prop];
+  //     }
+  //   }
+  //   shadowSize.X += offset;
+  //   shadowSize.Y += offset;
+  //   return shadowSize;
+  // },
+};
 
-  getShadow: function (offset) {
-    var shadowSize = {};
+var shadowOffset = 10;
 
-    for (var prop in this) {
-      if (typeof prop !== 'function') {
-        shadowSize[prop] = this[prop];
-      }
-    }
-    shadowSize.X += offset;
-    shadowSize.Y += offset;
-    return shadowSize;
-  },
+var shadowSize = {
+  X: cloudSize.X + shadowOffset,
+  Y: cloudSize.Y + shadowOffset,
+  WIDTH: cloudSize.WIDTH,
+  HEIGHT: cloudSize.HEIGHT,
 };
 
 var barSize = {
@@ -66,15 +73,15 @@ var renderCloud = function (ctx, cloud, color) {
 
 var renderText = function (ctx, txt, txtPosition, font, color) {
   ctx.fillStyle = color;
-  ctx.font = font.getFont();
+  ctx.font = fontStyle.SIZE + ' ' + fontStyle.FAMILY;
   ctx.textAlign = txtPosition.alignX;
   ctx.textBaseline = txtPosition.alignY;
   ctx.fillText(txt, txtPosition.x, txtPosition.y);
 };
 
-var renderBar = function (ctx, index, barScaleRate, name, time) {
+var renderBar = function (ctx, index, maxScale, name, time) {
   var textPosition = {};
-  var barHeight = time * barScaleRate;
+  var barHeight = Math.round(time * barSize.HEIGHT / maxScale);
   var barColor = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + getRandomPercent() + '%, 50%)';
 
   // столбик диаграммы
@@ -83,24 +90,21 @@ var renderBar = function (ctx, index, barScaleRate, name, time) {
 
   // время игрока
   textPosition.x = index * (barSize.GAP + barSize.WIDTH) + barSize.WIDTH / 2;
-  textPosition.y = barSize.HEIGHT - barHeight - fontStyle.getLineHeight();
+  textPosition.y = barSize.HEIGHT - barHeight - fontStyle.LINE_HEIGHT;
   textPosition.alignX = 'center';
   textPosition.alignY = 'top';
   renderText(ctx, Math.round(time), textPosition, fontStyle, COLOR_BLACK);
 
   // имя игрока
   textPosition.x = index * (barSize.GAP + barSize.WIDTH);
-  textPosition.y = barSize.HEIGHT + fontStyle.getLineHeight();
+  textPosition.y = barSize.HEIGHT + fontStyle.LINE_HEIGHT;
   textPosition.alignX = 'start';
   textPosition.alignY = 'bottom';
   renderText(ctx, name, textPosition, fontStyle, COLOR_BLACK);
 };
 
 var getMaxElement = function (arr) {
-  if (arr.length > 0) {
-    return Math.max.apply(null, arr);
-  }
-  return undefined;
+  return Math.max.apply(null, arr);
 };
 
 var getRandomPercent = function () {
@@ -122,7 +126,8 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.save();
 
   // нарисовать облако с тенью
-  renderCloud(ctx, cloudSize.getShadow(10), 'rgba(0, 0, 0, 0.7)');
+  // renderCloud(ctx, cloudSize.getShadow(10), 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, shadowSize, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, cloudSize, COLOR_WHITE);
 
   // вывести заголовок
@@ -132,20 +137,19 @@ window.renderStatistics = function (ctx, names, times) {
   renderText(ctx, lines[0], textPosition, fontStyle, COLOR_BLACK);
 
   textPosition.x = cloudSize.X + MARGIN_LEFT;
-  textPosition.y += fontStyle.getLineHeight();
+  textPosition.y += fontStyle.LINE_HEIGHT;
   textPosition.alignX = 'start';
   renderText(ctx, lines[1], textPosition, fontStyle, COLOR_BLACK);
 
   // перености начало координат в верхний левый угол диаграммы
-  ctx.translate(cloudSize.X + 2 * MARGIN_LEFT, cloudSize.Y + MARGIN_TOP + 3 * fontStyle.getLineHeight());
+  ctx.translate(cloudSize.X + 2 * MARGIN_LEFT, cloudSize.Y + MARGIN_TOP + 3 * fontStyle.LINE_HEIGHT);
 
   // определить параметры диаграммы
-  var maxTime = Math.round(getMaxElement(times));
-  var barScaleRate = barSize.HEIGHT / maxTime;
+  var maxTime = getMaxElement(times);
 
   // вывести диаграмму
   for (var i = 0; i < names.length; i++) {
-    renderBar(ctx, i, barScaleRate, names[i], times[i]);
+    renderBar(ctx, i, maxTime, names[i], times[i]);
   }
 
   // вернуть начальные значения
